06985151d8321abc206fa244f87837488a6a1545
Mohsin Raees <160493218+MohsinRaees@users.noreply.github.com>
2024-02-22 02:16:16 +0500

    Add files via upload

---
+++ DinnerGuest.ts
@@ -0,0 +1,6 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+et dinnerGuests: string[] = ["Mohsin", "Babar", "Salma", "Danial"];
+
+// Print the number of people invited to dinner
+console.log(`Number of people invited to dinner: ${dinnerGuests.length}`);
